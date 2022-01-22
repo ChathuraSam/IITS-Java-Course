0146b609c6fcc038c1e227b45d61da6e8fe2c63f
Chathura Samarajeewa <chathura.samarajeewa@gmail.com>
2022-01-22 12:53:00 +0530

    initial commit

---
+++ src/Variables.java
@@ -0,0 +1,24 @@
+public class Variables {
+    public static void main(String[] args) {
+        /*
+        primitive data types
+        --------------------
+        numerics - numbers
+            character(a - 65, b- 66, c-67, ENTER-123)
+                    char - 'C', '4', '#', '*'
+            integral
+                integers
+                    int - 23, 5678, 234
+                    byte
+                    short -
+                    long -
+
+                floating numbers - 234.234
+                    double
+                    float
+
+        boolean - true, false
+                    boolean
+        * */
+    }
+}
